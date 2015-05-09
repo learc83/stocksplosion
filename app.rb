47e34638136d1899a73fcf2ba67a47f53c8ded12
Seth Brown <learc83@gmail.com>
2015-05-09 17:00:59 -0400

    Refactored and cleaned up. Also formatted dates for display

--- app.rb
+++ app.rb
@@ -4,8 +4,7 @@ require 'json'
 require_relative 'api_access.rb'
 require_relative 'analyzer.rb'
 
-# Sinatra Routes
-
+#Routes
 get '/' do
   File.read(File.join('public', 'index.html'))
 end
@@ -27,26 +26,35 @@ get '/company_detail/:symbol/:created/:days_back' do |sym, created, days_back|
 	created_date = Date.parse(created)
 	start_date = Time.now.to_date - days_back.to_i
 
-	puts created_date
-	puts start_date
-
 	if created_date > start_date
 		return "{'error': Stock is too new to go back #{days_back} days.}"
 	end
 
+	generate_detail_response(sym, start_date)
+end
+
+
+#Helper Functions
+def generate_detail_response(sym, start_date)
 	response = ApiAccess.get_detail(sym, start_date)
 
 	json_hash = JSON.parse(response)
 	date_prices = json_hash['prices']
+	
 	#get just the prices
 	prices = date_prices.map {|r| r[1]}
 	
 	analyzer = Analyzer.new(prices)
+
+	#Strip year from dates
+	date_prices =  Hash[date_prices.map {|k,v| [to_month_day(k),v]}]
 
 	return_value = {'prices' => date_prices, 'decision' => analyzer.buy_sell_or_wait}
 
 	JSON.generate(return_value)
 end
 
-#Helper Functions
-
+def to_month_day(date)
+	new_date = Date.parse(date)
+	new_date.strftime("%m-%d")
+end
