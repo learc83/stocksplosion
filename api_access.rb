9561b509c9171409da1e442ca0009cb3459de5e7
Seth Brown <learc83@gmail.com>
2015-05-09 01:22:03 -0400

    refactored ApiAccess headers

--- api_access.rb
+++ api_access.rb
@@ -2,25 +2,21 @@ require 'rest-client'
 
 # We're using a class here solely for namespacing
 class ApiAccess
+	@@headers = {
+  		:content_type => 'application/json'
+	}
+
 	#FUTURE consider making these methods non-blocking, and consider
 	#caching responses
 	def self.list_companies
-		headers = {
-  			:content_type => 'application/json'
-		}
-
-		RestClient.get 'http://stocksplosion.apsis.io/api/company', headers
+		RestClient.get 'http://stocksplosion.apsis.io/api/company', @@headers
 	end
 
 	def self.get_detail(symbol, start_date)
-		headers = {
-  			:content_type => 'application/json'
-		}
-
 		now = Time.now
 		end_date = now.strftime("%Y%m%d")
 
 		RestClient.get "http://stocksplosion.apsis.io/api/company/#{symbol}"\
-			"?startdate=#{start_date}&enddate=#{end_date}", headers
+			"?startdate=#{start_date}&enddate=#{end_date}", @@headers
 	end
 end
