59617437504f8b4bfca190c5c44e7418631e125e
Seth Brown <learc83@gmail.com>
2015-05-08 22:25:27 -0400

    Use stock symbole directly for stock lookup instaed of id

--- app.rb
+++ app.rb
@@ -1,19 +1,22 @@
 require 'sinatra'
 
-#Sinatra Routes
+# Sinatra Routes
 
 get '/' do
   File.read(File.join('public', 'index.html'))
 end
 
+# Return entire list of symbols for autocomplete
+#TODO maybe return names as well
+#WARNING we might have to change strategies if the list grows too long
 get '/list_companies' do
 	'list of companies'
 end
 
-get '/company_detail/:id' do |id|
-	"company id: #{id}"
+get '/company_detail/:symbol' do |sym|
+	"company id: #{sym}"
 end
 
-get '/stock_decision/:id' do |id|
-	"buy company id: #{id}"
+get '/stock_decision/:symbol' do |sym|
+	"buy company id: #{sym}"
 end
