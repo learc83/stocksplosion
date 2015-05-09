c0bca95055a811572d0e6d240680e9ef86d00025
Seth Brown <learc83@gmail.com>
2015-05-08 22:19:22 -0400

    Added routing

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
 
-get '/hi' do
-  "Hello World!"
+#Sinatra Routes
+
+get '/' do
+  File.read(File.join('public', 'index.html'))
+end
+
+get '/list_companies' do
+	'list of companies'
+end
+
+get '/company_detail/:id' do |id|
+	"company id: #{id}"
+end
+
+get '/stock_decision/:id' do |id|
+	"buy company id: #{id}"
 end
