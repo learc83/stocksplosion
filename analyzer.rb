5fcc793cb1f3b6b612e0a416a01342c8dd3feb91
Seth Brown <learc83@gmail.com>
2015-05-09 14:34:02 -0400

    Analyzer init and simple moving avg calculator working

--- analyzer.rb
+++ analyzer.rb
@@ -1,3 +1,20 @@
 class Analyzer
+	def initialize(price_array)
+		error_str = "Not Enough Data for Analysis" 
+		raise ArgumentError.new(error_str) unless price_array.length > 2 
+		@price_array = price_array
+	end
 
+	#calculate the simple moving averge over last days_back. Using floating point
+	#shouldn't matter for the calculations we're doing since they don't rely on 
+	#high precision
+	def simple_moving_avg(days_back)
+		#get prices from n days_back
+		prices = @price_array[-days_back..-1]
+
+		#calculate SMA
+		prices.inject{ |sum, element| sum + element }.to_f / prices.size
+	end
+
+	
 end
--- test/test_analyzer.rb
+++ test/test_analyzer.rb
@@ -2,7 +2,25 @@ require_relative '../analyzer'
 require "test/unit"
 
 class TestAnalyzer < Test::Unit::TestCase
-	def test_test
-		assert_equal(true, false)
+	@@valid_price_array = [17057.15, 16894.07, 15899.8]
+	@@invalid_price_array = [17057.15, 16894.07]
+	@@sma_price_array = [50.0, 1.00, 2.00, 3.00]
+
+	def test_init 
+		assert_raise ArgumentError do 
+			Analyzer.new(@@invalid_price_array)
+		end
+
+		assert_nothing_raised ArgumentError do
+			Analyzer.new(@@valid_price_array)
+		end
 	end
+
+	def test_simple_moving_avg
+		analyzer = Analyzer.new(@@sma_price_array)
+
+		assert_equal(2, analyzer.simple_moving_avg(3))
+		assert_equal(2.5, analyzer.simple_moving_avg(2))
+	end
+
 end
